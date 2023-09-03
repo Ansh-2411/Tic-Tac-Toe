0a5b55544e0c368dc6351aee2ef6bcf3e7e86385
Ansh-2411 <anshsardhara24@gmail.com>
2023-09-03 20:42:18 +0530

    tic-tac-toe

---
+++ config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBWXG5TqRu_IvkpvpIdIzX-f9rhvx6wi4U",
+    authDomain: "tic-tac-toe-ea810.firebaseapp.com",
+    projectId: "tic-tac-toe-ea810",
+    storageBucket: "tic-tac-toe-ea810.appspot.com",
+    messagingSenderId: "618077377793",
+    appId: "1:618077377793:web:1213df137f93bd1713f700",
+    measurementId: "G-30TET1QBXC"
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
